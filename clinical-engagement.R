6c9155197668d5f453fdb415272a58c0269910e6
Michael Cronin <mfmcronin96@gmail.com>
2020-04-14 11:42:25 -0500

    added the trackers for referrals

--- clinical-engagement.R
+++ clinical-engagement.R
@@ -8,6 +8,7 @@ library(ergm)
 #Initialize function
 
 clinical_engagement <- function(net.f){
+  #this function simulates clinic visits
   
   ## get individual attributes 
   pop_size <- network.size(net.f)
@@ -22,48 +23,54 @@ clinical_engagement <- function(net.f){
   diagnosis <- net.f %v% "diagnosis"
   bc_status <- net.f %v% "bc_status"
   diagnosis_time <- net.f %v% "diagnosis_time"
-  neighbornav <- net.f %v% "neighbornav"
+  neighbor_navigated <- net.f %v% "neighbor_navigated"
   neighborfp <- net.f %v% "neighborfp"
   navigated <- net.f %v% "navigated"
   antinavigated <- net.f %v% "antinavigated"
   screen_complete <- net.f %v% "screen_complete"
   dt_complete <- net.f %v% "diagnostic_test_complete"
+  diagnostic_referral_counter <- net.f %v% "diagnostic_referral_counter"
+  screening_referral_counter <- net.f %v% "screening_referral_counter"
   
   attrib_mtrx<-cbind(symptom.severity,
                      reg.pcp.visitor,
                      navigated,
                      antinavigated,
-                     neighbornav,
+                     neighbor_navigated,
                      neighborfp)
   
   all_agents<-which(net.f %v% "diagnosis" == 0)
+  
   for (agent in all_agents){
     agent_data<-attrib_mtrx[agent,]
-    #cat("clinical eng agent #:", agent, '\n')
-    #Stage 3: simulate pcp visits
     #first: symptomatic agents without referrals roll for dt referrals
     if(diagnostic_referral[agent]==0 &
        screening_referral[agent]==0 &
        ss[agent]>0){
        diagnostic_referral[agent]<-rbinom(1,1,prob(agent_data,"dt"))
+       diagnostic_referral_counter[agent]<-diagnostic_referral_counter[agent]+diagnostic_referral[agent]
     }
     
     #second: all agents without referrals roll for sm referrals
     if(diagnostic_referral[agent]==0 &
        screening_referral[agent]==0){
       screening_referral[agent]<-rbinom(1,1,prob(agent_data,"sm"))
+      screening_referral_counter[agent]<-screening_referral_counter[agent]+screening_referral[agent]
     }
     
-    #stage 4: simulate navigation
-    else if((navigated[agent]==0) &
-            (dt_complete[agent]==0) &
-            (screen_complete[agent]==0) &
-            (diagnostic_referral[agent]==1 |
-             screening_referral[agent]==1)
-            ){
-      navigated[agent]<-rbinom(1,1,.5) #currently default value
-    }
+    #simulate navigation
+   else if((navigated[agent]==0) &
+           (dt_complete[agent]==0) &
+           (screen_complete[agent]==0) &
+           (diagnostic_referral[agent]==1 |
+            screening_referral[agent]==1) &
+           (neighbor_navigated[agent]==1)
+           ){
+     navigated[agent]<-rbinom(1,1,0.37) #currently default value
+   }
   }
+  net.f %v% "diagnostic_referral_counter" <- diagnostic_referral_counter
+  net.f %v% "screening_referral_counter" <- screening_referral_counter
   net.f %v% "navigated" <- navigated
   net.f %v% "screening_referral" <- screening_referral
   net.f %v% "diagnostic_referral" <- diagnostic_referral
