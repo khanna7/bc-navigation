c950430957a7507c3e9a1e9e06585c91c8a7d723
Michael Cronin <mfmcronin96@gmail.com>
2020-04-14 11:31:27 -0500

    added the trackers and started the newID code but still needs some work

--- demography-reset.R
+++ demography-reset.R
@@ -24,24 +24,35 @@ demography <- function(net.f){
   
   #All cause mortality
   all_cause_mort <- array(NA, dim = 5000)
-  for (i in seq(length(active.nodes))){
-    if (age[active.nodes[i]] < 55){
+  for (i in seq(length(age))){
+    if (age[i] < 55){
       all_cause_mort[i] <- rbinom(1,1, ac.mort.50)
-    } else if (age[active.nodes[i]] < 60){
+    } else if (age[i] < 60){
       all_cause_mort[i] <- rbinom(1,1, ac.mort.55)
-    } else if (age[active.nodes[i]] < 65){
+    } else if (age[i] < 65){
       all_cause_mort[i] <- rbinom(1,1, ac.mort.60)
-    } else if (age[active.nodes[i]] < 70){
+    } else if (age[i] < 70){
       all_cause_mort[i] <- rbinom(1,1, ac.mort.65)
-    } else if (age[active.nodes[i]] < 75){
+    } else if (age[i] < 75){
       all_cause_mort[i] <- rbinom(1,1, ac.mort.70)
     }
   }
+  
   all_cause_ids <- which(all_cause_mort == 1)
   
   nodes.to.reset <-union(all_cause_ids,age_mortality)
   nodes.to.reset <-union(nodes.to.reset,bc_mortality)
   nintros<-length(nodes.to.reset)
+  
+  
+  #####Ongoing project to change vertex IDs
+  #vertex.ids<-net.f %v% "vertex.names"
+  #
+  #for (i in 1:length(nodes.to.reset)){
+  #  vertex.ids[nodes.to.reset]
+  #}
+  #####Ongoing project to change vertex IDs
+  
   cat(nintros, "new agents", "\n")
   if(nintros>0){
     
@@ -51,7 +62,8 @@ demography <- function(net.f){
     set.vertex.attribute(net.f, "type", sample(c("n.ego", "n.alter"),1, prob = c(.1, .9)), nodes.to.reset)
     
     ##age
-    set.vertex.attribute(net.f, "age", min.age, nodes.to.reset) #should age be randomized
+    reset_ages<-sample(50:74,length(nodes.to.reset),replace=T) #basic randomization.
+    set.vertex.attribute(net.f, "age", reset_ages, nodes.to.reset)
     
     ## race: 100% of egos are black (=1); 95% of alters are black, 5% are other (=1) 
     set.vertex.attribute(net.f, "race", 1,nodes.to.reset)
@@ -62,7 +74,7 @@ demography <- function(net.f){
     set.vertex.attribute(net.f, "bmi.ge.30", rbinom(n, 1, prop.bmi.ge.30), nodes.to.reset)
     
     #menopausal status
-    set.vertex.attribute(net.f, "meno.status", 0, nodes.to.reset )
+    set.vertex.attribute(net.f, "meno.status", 0, nodes.to.reset)
     
     #First degree relative history
     set.vertex.attribute(net.f, "fd.rel", 0, nodes.to.reset)
@@ -113,6 +125,12 @@ demography <- function(net.f){
     set.vertex.attribute(net.f, "antinavigated", 0, nodes.to.reset)
     set.vertex.attribute(net.f, "screen_complete", 0, nodes.to.reset)
     set.vertex.attribute(net.f, "diagnostic_test_complete", 0, nodes.to.reset)
+    set.vertex.attribute(net.f, "screen_result_roll", 0, nodes.to.reset)
+    set.vertex.attribute(net.f, "neighborfp_roll", 0, nodes.to.reset)
+    set.vertex.attribute(net.f, "diagnostic_referral_counter", 0, nodes.to.reset)
+    set.vertex.attribute(net.f, "screening_referral_counter", 0, nodes.to.reset)
+    set.vertex.attribute(net.f, "diagnostic_visit_counter", 0, nodes.to.reset)
+    set.vertex.attribute(net.f, "screening_visit_counter", 0, nodes.to.reset)
     
     #set vertex attribute for whether a patient has been diagnosed
     set.vertex.attribute(net.f, "diagnosis", 0, nodes.to.reset)
@@ -128,11 +146,9 @@ demography <- function(net.f){
   }
   
   # Save object -----
-  alive_agents<-which(is.active(net.f, v = 1:network.size(net.f), at = time))
-  deceased_agents<-which(!is.active(net.f, v = 1:network.size(net.f), at = time))
-  number.of.alive.agents<-length(alive_agents)
-  number.of.deceased.agents<-length(deceased_agents)
-  number.of.positive.bc.agents<-length(intersect(which(net.f %v% "bc_status"==1),alive_agents))
+  number.of.positive.bc.agents<-length(which(net.f %v% "bc_status"==1))
+  number.of.hpos.agents<-length(which(net.f %v% "subtype"==1))
+  number.of.hneg.agents<-length(which(net.f %v% "subtype"==2))
   number.of.diagnosed.cases<-length(which(net.f %v% "diagnosis"==1))
   number.of.navigated.agents<-length(which(net.f %v% "navigated"==1))
   
@@ -143,16 +159,20 @@ demography <- function(net.f){
   number.of.dt.completed<-length(which(net.f %v% "diagnostic_test_complete"==1))
   
   number.of.symptomatic<-length(which(net.f %v% "symptom.severity">0))
-  #length.of.disease.time<-length(which(net.f %v% "disease">0)))
-  #number.of.
+  
+  positives<-which(net.f %v% "bc_status"==1)
+  diagnosed<-which(net.f %v% "diagnosis"==1)
+  disease.time<-net.f %v% "disease.time"
+  time.with.cancer<-mean(disease.time[positives])
+  diagnosis_time<-net.f %v% "disease.time"
+  time.until.diagnosis<-mean(diagnosis_time[diagnosed])
   
   write.table(cbind(time,
-                    nintros, #births
-                    number.of.alive.agents,
-                    number.of.deceased.agents,
+                    nintros, #deaths
                     number.of.positive.bc.agents,
+                    number.of.hpos.agents,
+                    number.of.hneg.agents,
                     number.of.diagnosed.cases,
-                    number.of.navigated.agents,
                     
                     number.of.diagnostic.referrals,
                     number.of.screening.referrals,
@@ -160,8 +180,12 @@ demography <- function(net.f){
                     number.of.screen.completed,
                     number.of.dt.completed,
                     
-                    number.of.symptomatic),
-              file="bc.dem.burnin.fixed5000.for360.social_off.unipartite.data", ## to note total number of new infections
+                    number.of.symptomatic,
+                    number.of.navigated.agents,
+                    
+                    time.with.cancer,
+                    time.until.diagnosis),
+              file="2bc.dem.burnin.fixed5000.for360.social_off.unipartite.data", ## to note total number of new infections
               append=TRUE,
               col.names=FALSE,
               row.names=FALSE
